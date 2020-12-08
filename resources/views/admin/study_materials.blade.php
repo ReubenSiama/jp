6f6a622ac7d9ea64e1a75209d40df3a23ce240c5
ReubenSiama <reubenlalhmunsiama@gmail.com>
2020-12-08 17:28:38 +0530

    progress

--- resources/views/admin/study_materials.blade.php
+++ resources/views/admin/study_materials.blade.php
@@ -188,7 +188,7 @@
 </script>
 {{-- <script src="https://malsup.github.com/jquery.form.js"></script> --}}
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/4.3.0/jquery.form.min.js" integrity="sha384-qlmct0AOBiA2VPZkMY3+2WqkHtIQ9lSdAsAn5RUJD/3vA5MKDgSGcdmIv4ycVxyn" crossorigin="anonymous"></script>
-{{-- <script>
+<script>
 
 (function() {
     var bar = $('.bar');
@@ -219,6 +219,6 @@
         }
     }); 
 })();
-</script> --}}
+</script>
 
 @endsection
